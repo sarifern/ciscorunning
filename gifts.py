69d4e7e435606e097dba6472961992b525ab657e
Sari Fernandez <sarifern@cisco.com>
2022-12-05 12:01:24 -0600

    kickoff prize test

--- gifts.py
+++ gifts.py
@@ -3,31 +3,7 @@ from django.core.exceptions import MultipleObjectsReturned
 from ic_marathon_app.views import check_badges
 
 cecs_who_joined = [
-"noferrer",
-"alegarc2",
-"maespond",
-"vbustama",
-"gpenaloz",
-"bzendeja",
-"josgonz5",
-"msosagar",
-"baraiza",
-"drincond",
-"aifrias",
-"sairodri",
-"gedurazo",
-"erikfran",
-"rovazque",
-"danielan",
-"alberthe",
-"maydomin",
-"lgomezro",
-"fchongce",
-"balvirde",
-"olopezcr",
-"mcerqued",
-"alejsant",
-"pgarduno"
+"sarifern"
 ]
 
 for cec in cecs_who_joined:
