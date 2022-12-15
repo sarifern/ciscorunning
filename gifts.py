f4bc2aa18b0679f0fe7aed17793105c2566d2cb8
Alf-Prado <alfpradoc@gmail.com>
2022-12-15 16:17:35 -0600

    Adding particpants for 5Km gift

--- gifts.py
+++ gifts.py
@@ -2,9 +2,12 @@ from ic_marathon_app.models import Profile
 from django.core.exceptions import MultipleObjectsReturned
 from ic_marathon_app.views import check_badges
 
-cecs_who_joined = [
+""" cecs_who_joined = [
 "apradoca"
-]
+] """
+
+cecs_who_joined = ['adlira', 'acalvill', 'adronqui', 'adserran', 'adrsoto', 'alamedra', 'alberthe', 'alsalvad', 'alegarc2', 'adias', 'avercor', 'anpliego', 'aniembro', 'aoscos', 'argamez', 'amezaara', 'artmoral', 'baraiza', 'balvirde', 'bguerram', 'karingon', 'bremarqu', 'ccarapai', 'cesaavil', 'claudipe', 'danrami2', 'dmeixuei', 'daviher2', 'drincond', 'edgarma', 'elopezlo', 'erichgon', 'eradillo', 'glalvara', 'fermunoz', 'flugo', 'gpenaloz', 'florodri', 'gubell', 'hemherna', 'ilgonzal', 'iguerram', 'jacramos', 'jesusiba', 'jorgavil', 'jfuente2', 'josgonz5', 'jlunapal', 'josencha', 'jdavilad', 'jmauleon', 'jaraizal', 'juliocro', 'kagodine', 'kasherna', 'kevsalaz', 'lauraher', 'lchaconz', 'anaarand', 'lucervan', 'lgomezro', 'manuejim', 'malamill', 'mariae', 'maribgue', 'maguila2', 'marsand3', 'jesusgo2', 'maareval', 'mcerqued', 'marizmen', 'gtomich', 'rrojaslo', 'nbecerri', 'ogandari', 'fmejiapa', 'rsanties', 'rebherna', 'rgalveza', 'rogestra', 'rovazque', 'romcleme', 'ronarosa', 'samanmar', 'sromeroz', 'sanespin', 'sebarive', 'vbustama', 'victorhs', 'vlara', 'yperezro', 'armangar', 'ysalaric', 'zespinos']
+
 
 for cec in cecs_who_joined:
     try:
