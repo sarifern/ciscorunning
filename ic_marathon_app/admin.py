6bda37ecec163e95b74684dee12d507b67c74b4f
Sari Fernandez <sarifern@cisco.com>
2023-12-22 11:19:50 -0600

    Taking out the Forgot password link. Prompting to ask Password recovery to the admins.

--- ic_marathon_app/admin.py
+++ ic_marathon_app/admin.py
@@ -5,8 +5,8 @@ from django.utils.html import format_html
 
 
 class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ('uploaded_at','belongs_to', 'distance', 'image_tag')
-    list_filter = ['belongs_to','uploaded_at']
+    list_display = ('uploaded_at', 'belongs_to', 'distance', 'image_tag')
+    list_filter = ['belongs_to', 'uploaded_at']
 
     def image_tag(self, obj):
         return format_html(
