68a9834af16f974c8592a4a16a64b759777cebab
Sari Fernandez Barbosa (sarifern) <sarifern@cisco.com>
2024-11-21 17:54:42 -0600

    Updating allowed_host to ciscorunning.com

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -31,7 +31,7 @@ if os.environ.get("DEBUG_PREF") != "False":
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "ciscorunning.xyz","www.ciscorunning.xyz", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "ciscorunning.com","www.ciscorunning.com", ".herokuapp.com"]
 
 
 DJANGO_APPS = [
