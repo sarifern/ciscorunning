50af775b5f5825c227e72287a4f86214be071ddd
Sari Fernandez <sarifern@cisco.com>
2024-12-10 22:14:35 -0600

    extending to Jan 8th

--- ic_marathon_app/views.py
+++ ic_marathon_app/views.py
@@ -20,7 +20,7 @@ WTAPI = WebexTeamsAPI(access_token=os.environ.get("WT_TOKEN"))
 DATE_START = datetime(2024, 12, 11, 1, 0, 0).replace(
     tzinfo=tz.timezone("America/Mexico_City")
 )
-DATE_END = datetime(2025, 1, 7, 0, 0, 0).replace(
+DATE_END = datetime(2025, 1, 8, 0, 0, 0).replace(
     tzinfo=tz.timezone("America/Mexico_City")
 )
 
