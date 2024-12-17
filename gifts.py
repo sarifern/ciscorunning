2223184eff228bf9ca31ef9e325a06f0cdf8f818
Sari Fernandez <sarifern@cisco.com>
2024-12-17 11:55:03 -0600

    extended gifts.py

--- gifts.py
+++ gifts.py
@@ -4,10 +4,12 @@ from ic_marathon_app.views import check_badges, WTAPI
 import os
 import datetime
 
+
 def divide_into_batches(input_list, batch_size):
     """Divide a list into batches of a specified size."""
     for i in range(0, len(input_list), batch_size):
-        yield input_list[i:i + batch_size]
+        yield input_list[i : i + batch_size]
+
 
 cecs_who_joined = os.environ.get("CECS_WHO_JOINED").split(",")
 
@@ -31,6 +33,10 @@ for cec in cecs_who_joined:
         # check profile manually
         print(f"duplicated user {cec}, check manually")
     except Profile.DoesNotExist:
+        WTAPI.messages.create(
+            toPersonEmail=f"{cec}@cisco.com",
+            markdown=f"Hey, you still haven't registered at www.ciscorunning.com. Please complete your profile. Once you have completed your registration, contact 'gpe-reyes-marathon@cisco.com' to get your extra KM.",
+        )
         print(f"profile for user {cec} does not exist")
         pass
 
