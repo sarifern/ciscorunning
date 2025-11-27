6986a1bc84ae12108c4130161946ed1106731925
Sari Fernandez <sarifern@cisco.com>
2025-11-27 10:45:46 -0600

    fix: Revert S3 region to us-east-1 for ciscorunning2023 bucket

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -203,7 +203,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
-AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_REGION_NAME = "us-east-1"
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 
 # Force AWS Signature Version 4 (required for newer buckets and regions)
